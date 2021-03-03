806cc746efc46099c781a34ce1dad6df302fbddc
JyotiSunkara <jyoti.sunkara@students.iiit.ac.in>
2021-03-03 08:45:40 +0530

    Basic CMake

---
+++ build/CMakeFiles/InitGL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InitGL.dir/source/main.cpp.o"
+  "InitGL"
+  "InitGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InitGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
